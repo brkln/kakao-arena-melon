59c1ad447b41a53bc2d373ddeaeaa5325f6d3efa
brkln <sangjoon23@naver.com>
2020-07-23 14:12:20 +0900

    Refactor infer class

--- inference.py
+++ inference.py
@@ -1,72 +1,61 @@
 # -*- coding: utf-8 -*-
-from collections import Counter
-
 import fire
-from tqdm import tqdm
-
-from arena_util import load_json
+import numpy as np
+import pandas as pd
+import pickle
+from scipy import sparse
+import random
+import implicit
 from arena_util import write_json
 from arena_util import remove_seen
-from arena_util import most_popular
 
+random.seed(0)
 
-class GenreMostPopular:
-    def _song_mp_per_genre(self, song_meta, global_mp):
-        res = {}
+class Infer:
+    def _generate_answers(self, val):
+        # 0
+        popular_num = 10000
+        trial = 23
+        # 1
+        with open("popular_song_dict.pkl", "rb") as f:
+            popular_song_dict = pickle.load(f)
+        with open("popular_tag_dict.pkl", "rb") as f:
+            popular_tag_dict = pickle.load(f)
+        with open("trainval_id_dict.pkl", "rb") as f:
+            trainval_id_dict = pickle.load(f)
+        songtag_matrix = sparse.load_npz('songtag_matrix_{}.npz'.format(trial))
+        with open('model_{}.sav'.format(trial), 'rb') as f:
+            model = pickle.load(f)
 
-        for sid, song in song_meta.items():
-            for genre in song['song_gn_gnr_basket']:
-                res.setdefault(genre, []).append(sid)
+        print("done 1")
 
-        for genre, sids in res.items():
-            res[genre] = Counter({k: global_mp.get(int(k), 0) for k in sids})
-            res[genre] = [k for k, v in res[genre].most_common(200)]
+        song_fill = []
+        tag_fill = []
+        for j in [trainval_id_dict[i] for i in val.id.values]:
+            song_fill.append([popular_song_dict[k] for k in [i for i,_ in model.rank_items(j, songtag_matrix, list(range(popular_num)))[:200]]])
+            tag_fill.append([popular_tag_dict[k] for k,_ in model.recommend(j, songtag_matrix, filter_items = range(popular_num), N = 15)])
 
-        return res
-
-    def _generate_answers(self, song_meta_json, train, questions):
-        song_meta = {int(song["id"]): song for song in song_meta_json}
-        song_mp_counter, song_mp = most_popular(train, "songs", 200)
-        tag_mp_counter, tag_mp = most_popular(train, "tags", 100)
-        song_mp_per_genre = self._song_mp_per_genre(song_meta, song_mp_counter)
-
+        print("done 2")
+    
         answers = []
-        for q in tqdm(questions):
-            genre_counter = Counter()
-
-            for sid in q["songs"]:
-                for genre in song_meta[sid]["song_gn_gnr_basket"]:
-                    genre_counter.update({genre: 1})
-
-            top_genre = genre_counter.most_common(1)
-
-            if len(top_genre) != 0:
-                cur_songs = song_mp_per_genre[top_genre[0][0]]
-            else:
-                cur_songs = song_mp
-
+        for i in range(len(val)):
             answers.append({
-                "id": q["id"],
-                "songs": remove_seen(q["songs"], cur_songs)[:100],
-                "tags": remove_seen(q["tags"], tag_mp)[:10]
+                "id": val.id.values[i],
+                "songs": remove_seen(val.songs.values[i], song_fill[i])[:100],
+                "tags": remove_seen(val.tags.values[i], tag_fill[i])[:10]
             })
+            
+        print("done 3")
 
         return answers
 
-    def run(self, song_meta_fname, train_fname, question_fname):
-        print("Loading song meta...")
-        song_meta_json = load_json(song_meta_fname)
-
-        print("Loading train file...")
-        train_data = load_json(train_fname)
-
+    def run(self, question_fname):
         print("Loading question file...")
-        questions = load_json(question_fname)
+        questions = pd.read_json(question_fname)
 
         print("Writing answers...")
-        answers = self._generate_answers(song_meta_json, train_data, questions)
+        answers = self._generate_answers(questions)
         write_json(answers, "results/results.json")
 
-
 if __name__ == "__main__":
-    fire.Fire(GenreMostPopular)
+    fire.Fire(Infer)
