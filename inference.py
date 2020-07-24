b17c25dcc24ae00653945045e046a46db3f43b17
brkln <sangjoon23@naver.com>
2020-07-24 13:50:50 +0900

    Give a variation to songtag_matrix by specifying popular_song

--- inference.py
+++ inference.py
@@ -15,7 +15,7 @@ class Infer:
     def _generate_answers(self, val):
         # 0
         popular_num = 615142
-        trial = 29
+        trial = 30
         # 1
         with open("popular_song_dict.pkl", "rb") as f:
             popular_song_dict = pickle.load(f)
@@ -59,4 +59,3 @@ class Infer:
 
 if __name__ == "__main__":
     fire.Fire(Infer)
-    
--- train.py
+++ train.py
@@ -14,7 +14,7 @@ from arena_util import most_popular
 random.seed(0)
 
 class Train:
-    def get_token(self, title, tokenizer):     # get_token 함수 출처: https://arena.kakao.com/forum/topics/226
+    def get_token(self, title, tokenizer):        # get_token 함수 출처: https://arena.kakao.com/forum/topics/226
         if len(title)== 0 or title== ' ' or title == '\u3000\u3000\u3000\u3000\u3000\u3000\u3000\u3000':
             return []
 
@@ -25,10 +25,15 @@ class Train:
     def _train(self, train_json, train, val):
         # 0
         total_num = 707989
-        popular_num = 615142
-        trial = 29
+        popular_num = 615142        # train 데이터 내 곡 개수 615142개
+        trial = 30
         # 1
         _, popular_song = most_popular(train_json, 'songs', popular_num)
+        song_100 = popular_song[:100]
+        song_200 = popular_song[100:200]
+        song_300 = popular_song[200:300]
+        song_400 = popular_song[300:400]
+        song_500 = popular_song[400:500]
 
         popular_song_dict = {}
         for i,j in enumerate(sorted(popular_song)):
@@ -88,7 +93,20 @@ class Train:
         for l in trainval.songtag:
             cols.extend(l)
 
-        data = [1] * len(cols)
+        data = []
+        for i in cols:
+            if i in song_100:
+                data.append(6)
+            elif i in song_200:
+                data.append(5)
+            elif i in song_300:
+                data.append(4)
+            elif i in song_400:
+                data.append(3)
+            elif i in song_500:
+                data.append(2)
+            else:
+                data.append(1)
 
         songtag_matrix = sparse.csr_matrix((data, (rows, cols)))
         songtag_matrix = songtag_matrix[sorted(set(trainval.id.values)), :]
@@ -96,7 +114,7 @@ class Train:
 
         sparse.save_npz('songtag_matrix_{}.npz'.format(trial), songtag_matrix)
 
-        model = implicit.als.AlternatingLeastSquares(iterations = 150)
+        model = implicit.als.AlternatingLeastSquares()
         model.fit(songtag_matrix.T)
 
         with open('model_{}.sav'.format(trial), 'wb') as f:
@@ -117,4 +135,3 @@ class Train:
 
 if __name__ == "__main__":
     fire.Fire(Train)
-    
