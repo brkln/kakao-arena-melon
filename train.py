1032f3b304895a194efb029efad341fffc3e4425
brkln <sangjoon23@naver.com>
2020-07-26 01:24:13 +0900

    Adjust intervals

--- train.py
+++ train.py
@@ -104,17 +104,17 @@ class Train:
                 elif i in popular_song[50:150]:
                     data.append(130)
                 elif i in popular_song[150:300]:
-                    data.append(140)
-                elif i in popular_song[300:500]:
                     data.append(150)
-                elif i in popular_song[500:700]:
-                    data.append(160)
-                elif i in popular_song[700:1000]:
-                    data.append(170)
-                elif i in popular_song[1000:1500]:
+                elif i in popular_song[300:500]:
                     data.append(180)
+                elif i in popular_song[500:700]:
+                    data.append(220)
+                elif i in popular_song[700:1000]:
+                    data.append(270)
+                elif i in popular_song[1000:1500]:
+                    data.append(330)
                 elif i in popular_song[1500:2000]:
-                    data.append(190)
+                    data.append(400)
                 # elif i in popular_song[80:450]:
                 #     data.append(90)
                 # elif i in popular_song[90:550]:
@@ -140,7 +140,7 @@ class Train:
                 # elif i in popular_song[190:200]:
                 #     data.append(20)
                 else:
-                    data.append(200)
+                    data.append(480)
             else:
                 if i in popular_tag[:50]:
                     data.append(100)
@@ -192,4 +192,3 @@ class Train:
 
 if __name__ == "__main__":
     fire.Fire(Train)
-    
