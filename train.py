716862fb23b30228da6f2e3d14766b8ce59dde46
brkln <sangjoon23@naver.com>
2020-07-25 22:30:38 +0900

    Adjust intervals

--- train.py
+++ train.py
@@ -100,70 +100,70 @@ class Train:
         for i in cols:
             if i < total_num:
                 if i in popular_song[:10]:
-                    data.append(10)
-                elif i in popular_song[10:20]:
-                    data.append(20)
-                elif i in popular_song[20:30]:
-                    data.append(30)
-                elif i in popular_song[30:40]:
-                    data.append(40)
-                elif i in popular_song[40:50]:
-                    data.append(50)
-                elif i in popular_song[50:60]:
-                    data.append(60)
-                elif i in popular_song[60:70]:
-                    data.append(70)
-                elif i in popular_song[70:80]:
-                    data.append(80)
-                elif i in popular_song[80:90]:
-                    data.append(90)
-                elif i in popular_song[90:100]:
-                    data.append(100)
-                elif i in popular_song[100:110]:
-                    data.append(110)
-                elif i in popular_song[110:120]:
-                    data.append(100)
-                elif i in popular_song[120:130]:
-                    data.append(90)
-                elif i in popular_song[130:140]:
-                    data.append(80)
-                elif i in popular_song[140:150]:
-                    data.append(70)
-                elif i in popular_song[150:160]:
-                    data.append(60)
-                elif i in popular_song[160:170]:
-                    data.append(50)
-                elif i in popular_song[170:180]:
-                    data.append(40)
-                elif i in popular_song[180:190]:
-                    data.append(30)
-                elif i in popular_song[190:200]:
-                    data.append(20)
+                    data.append(100000)
+                # elif i in popular_song[10:20]:
+                #     data.append(20)
+                # elif i in popular_song[20:30]:
+                #     data.append(30)
+                # elif i in popular_song[30:40]:
+                #     data.append(40)
+                # elif i in popular_song[40:50]:
+                #     data.append(50)
+                # elif i in popular_song[50:60]:
+                #     data.append(60)
+                # elif i in popular_song[60:70]:
+                #     data.append(70)
+                # elif i in popular_song[70:80]:
+                #     data.append(80)
+                # elif i in popular_song[80:90]:
+                #     data.append(90)
+                # elif i in popular_song[90:100]:
+                #     data.append(100)
+                # elif i in popular_song[100:110]:
+                #     data.append(110)
+                # elif i in popular_song[110:120]:
+                #     data.append(100)
+                # elif i in popular_song[120:130]:
+                #     data.append(90)
+                # elif i in popular_song[130:140]:
+                #     data.append(80)
+                # elif i in popular_song[140:150]:
+                #     data.append(70)
+                # elif i in popular_song[150:160]:
+                #     data.append(60)
+                # elif i in popular_song[160:170]:
+                #     data.append(50)
+                # elif i in popular_song[170:180]:
+                #     data.append(40)
+                # elif i in popular_song[180:190]:
+                #     data.append(30)
+                # elif i in popular_song[190:200]:
+                #     data.append(20)
                 else:
-                    data.append(10)
+                    data.append(100000)
             else:
                 if i in popular_tag[:10]:
-                    data.append(10)
-                elif i in popular_tag[10:20]:
-                    data.append(20)
-                elif i in popular_tag[20:30]:
-                    data.append(30)
-                elif i in popular_tag[30:40]:
-                    data.append(40)
-                elif i in popular_tag[40:50]:
-                    data.append(50)
-                elif i in popular_tag[50:60]:
-                    data.append(60)
-                elif i in popular_tag[60:70]:
-                    data.append(50)
-                elif i in popular_tag[70:80]:
-                    data.append(40)
-                elif i in popular_tag[80:90]:
-                    data.append(30)
-                elif i in popular_tag[90:100]:
-                    data.append(20)
+                    data.append(100000)
+                # elif i in popular_tag[10:20]:
+                #     data.append(20)
+                # elif i in popular_tag[20:30]:
+                #     data.append(30)
+                # elif i in popular_tag[30:40]:
+                #     data.append(40)
+                # elif i in popular_tag[40:50]:
+                #     data.append(50)
+                # elif i in popular_tag[50:60]:
+                #     data.append(60)
+                # elif i in popular_tag[60:70]:
+                #     data.append(50)
+                # elif i in popular_tag[70:80]:
+                #     data.append(40)
+                # elif i in popular_tag[80:90]:
+                #     data.append(30)
+                # elif i in popular_tag[90:100]:
+                #     data.append(20)
                 else:
-                    data.append(10)
+                    data.append(100000)
 
         songtag_matrix = sparse.csr_matrix((data, (rows, cols)))
         songtag_matrix = songtag_matrix[sorted(set(trainval.id.values)), :]
